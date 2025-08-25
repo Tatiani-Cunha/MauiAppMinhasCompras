c9f70db24e25ac72940c625e34b84615069d8157
Tatiani Cunha <tatianicpsantana@gmail.com>
2025-08-25 00:00:00 -0300

    Commit  de modificação da View Novo Produto e Lista de Produto

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,9 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Maui.Controls;
+
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +12,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso", "Produto inserido com sucesso!", "OK");
+
+        } catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
