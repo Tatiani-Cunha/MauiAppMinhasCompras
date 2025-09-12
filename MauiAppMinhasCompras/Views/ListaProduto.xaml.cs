ef231ac844f2417afbb830226b4a20b9d172442d
Tatiani Cunha <tatianicpsantana@gmail.com>
2025-09-11 23:38:54 -0300

    Melhorias da Interface Grafica

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -7,12 +7,12 @@ namespace MauiAppMinhasCompras.Views;
 public partial class ListaProduto : ContentPage
 {
 	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
-    public ListaProduto()
+	public ListaProduto()
 	{
 		InitializeComponent();
 
 		lst_produtos.ItemsSource = lista;
-    }
+	}
 
 	protected async override void OnAppearing()
 	{
@@ -20,29 +20,30 @@ public partial class ListaProduto : ContentPage
 		{
 			lista.Clear();
 
-            List<Produto> tmp = await App.Db.GetAll();
+			List<Produto> tmp = await App.Db.GetAll();
 
 			tmp.ForEach(i => lista.Add(i));
 		}
 		catch (Exception ex)
 		{
 			await DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
+		}
+	}
 
 
-    private void ToolbarItem_Clicked(object sender, EventArgs e)
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 		try
 		{
 			Navigation.PushAsync(new Views.NovoProduto());
 
-        } catch (Exception ex)
+		}
+		catch (Exception ex)
 		{
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
 
-    }
+	}
 
 	private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
 	{
@@ -50,7 +51,9 @@ public partial class ListaProduto : ContentPage
 		{
 			string q = e.NewTextValue;
 
-			lista.Clear();
+            lst_produtos.IsRefreshing = true;
+
+            lista.Clear();
 
 			List<Produto> tmp = await App.Db.Search(q);
 
@@ -59,21 +62,25 @@ public partial class ListaProduto : ContentPage
 		catch (Exception ex)
 		{
 			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+		finally
+		{
+			lst_produtos.IsRefreshing = false;
         }
 
-    }
+	}
 
-    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
-    {
+	private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+	{
 		double soma = lista.Sum(i => i.Total);
 
 		string msg = $"O total é {soma:C}";
 
 		DisplayAlert("Total dos Produtos", msg, "OK");
-    }
+	}
 
-    private async void MenuItem_Clicked(object sender, EventArgs e)
-    {
+	private async void MenuItem_Clicked(object sender, EventArgs e)
+	{
 		try
 		{
 			MenuItem selecionado = sender as MenuItem;
@@ -82,21 +89,21 @@ public partial class ListaProduto : ContentPage
 
 			bool confirm = await DisplayAlert(
 				"Tem certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
-			
+
 			if (confirm)
 			{
 				await App.Db.Delete(p.ID);
 				lista.Remove(p);
-            }
-        }
+			}
+		}
 		catch (Exception ex)
 		{
 			await DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
+		}
+	}
 
-    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-    {
+	private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+	{
 		try
 		{
 			Produto p = e.SelectedItem as Produto;
@@ -104,11 +111,31 @@ public partial class ListaProduto : ContentPage
 			Navigation.PushAsync(new Views.EditarProduto
 			{
 				BindingContext = p,
-            });
-        }
+			});
+		}
 		catch (Exception ex)
 		{
-            DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
+
+	private async void lst_produtos_Refreshing(object sender, EventArgs e)
+	{
+		try
+		{
+			lista.Clear();
+
+			List<Produto> tmp = await App.Db.GetAll();
+
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+		finally
+		{
+			lst_produtos.IsRefreshing = false;
+		}
+	}
 }
